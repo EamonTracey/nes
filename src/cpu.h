48a76fd82b4f6ebd7abec0d1c3fd44aea28b85b7
eamontracey <etracey@nd.edu>
2024-06-09 16:51:23 -0700

    Add instruction functions

--- src/cpu.h
+++ src/cpu.h
@@ -311,6 +311,64 @@ class Cpu6502 {
     // Power on the CPU. This initializes register and memory contents.
     void Power();
 
+    // Instruction functions.
+    void ADC();
+    void AND();
+    void ASL();
+    void BCC();
+    void BCS();
+    void BEQ();
+    void BIT();
+    void BMI();
+    void BNE();
+    void BPL();
+    void BRK();
+    void BVC();
+    void BVS();
+    void CLC();
+    void CLD();
+    void CLI();
+    void CLV();
+    void CMP();
+    void CPX();
+    void CPY();
+    void DEC();
+    void DEX();
+    void DEY();
+    void EOR();
+    void INC();
+    void INX();
+    void INY();
+    void JMP();
+    void JSR();
+    void LDA();
+    void LDX();
+    void LDY();
+    void LSR();
+    void NOP();
+    void ORA();
+    void PHA();
+    void PHP();
+    void PLA();
+    void PLP();
+    void ROL();
+    void ROR();
+    void RTI();
+    void RTS();
+    void SBC();
+    void SEC();
+    void SED();
+    void SEI();
+    void STA();
+    void STX();
+    void STY();
+    void TAX();
+    void TAY();
+    void TSX();
+    void TXA();
+    void TXS();
+    void TYA();
+
     // The 6502 features three general-purpose registers, a status register, a
     // stack pointer, and a program counter.
     uint8_t accumulator;
